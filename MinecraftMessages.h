29016f7f9ca8427ece7575c6a9e1a4f0259e5014
Joachim Bengtsson <joachimb@gmail.com>
2010-11-10 23:56:42 +0100

    More Minecraft messages, and separating c>s and s>c messages

--- MinecraftMessages.h
+++ MinecraftMessages.h
@@ -1,10 +1,54 @@
 #import "AutoProto.h"
 
-@interface LoginRequest : APMessage
+// http://mc.kev009.com/wiki/Protocol
+
+#pragma mark Client-to-server
+
+@interface CSKeepAlive : APMessage
+@end
+
+@interface CSLoginRequest : APMessage
 @property uint32_t protocolVersion;
-@property (retain) NSString *username, *password;
+@property (copy) NSString *username, *password;
 @property uint64_t mapSeed;
 @property uint8_t dimension;
 @end
 
-Class MinecraftMessageFactory(uint8_t packetId);
+@interface CSHandshake : APMessage
+@property (copy) NSString *username;
+@end
+
+@interface CSChatMessage : APMessage
+@property (copy) NSString *message;
+@end
+
+Class MinecraftMessageFactoryCS(uint8_t packetId);
+
+
+#pragma mark 
+#pragma mark Server-to-client
+
+@interface SCKeepAlive : APMessage
+@end
+
+@interface SCLoginResponse : APMessage
+@property uint32_t unknown;
+@property (copy) NSString *serverNameMaybe;
+@property (copy) NSString *MOTDMaybe;
+@property uint64_t mapSeed;
+@property uint8_t dimension;
+@end
+
+@interface SCHandshake : APMessage
+@property (copy) NSString *connectionHash;
+@end
+
+@interface SCChatMessage : APMessage
+@property (copy) NSString *message;
+@end
+
+@interface SCTimeUpdate : APMessage
+@property uint64_t timeInMinutes;
+@end
+
+Class MinecraftMessageFactorySC(uint8_t packetId);
--- MinecraftMessages.m
+++ MinecraftMessages.m
@@ -1,15 +1,78 @@
 #import "MinecraftMessages.h"
 
-@implementation LoginRequest
+#pragma mark 
+#pragma mark Client-to-server
+
+@implementation CSKeepAlive
++(uint8_t)packetId; { return 0x00; }
+@end
+@implementation CSLoginRequest
++(uint8_t)packetId; { return 0x01; }
 @synthesize protocolVersion, username, password, mapSeed, dimension;
 @end
+@implementation CSHandshake
++(uint8_t)packetId; { return 0x02; }
+@synthesize username;
+@end
+@implementation CSChatMessage
++(uint8_t)packetId; { return 0x03; }
+@synthesize message;
+@end
 
-Class MinecraftMessageFactory(uint8_t packetId)
+#define insert(cls) messages[[cls packetId]] = [cls class]
+
+
+Class MinecraftMessageFactoryCS(uint8_t packetId)
 {
 	static Class messages[0xff] = {Nil};
 	static BOOL inited = NO;
 	if(!inited) {
-		messages[0x01] = [LoginRequest class];
+		insert(CSKeepAlive);
+		insert(CSLoginRequest);
+		insert(CSHandshake);
+		insert(CSChatMessage);
+		inited = YES;
+	}
+	return messages[packetId];
+}
+
+#pragma mark
+#pragma mark Server-to-client
+
+@implementation SCKeepAlive
++(uint8_t)packetId; { return 0x00; }
+@end
+
+@implementation SCLoginResponse
++(uint8_t)packetId; { return 0x01; }
+@synthesize unknown, serverNameMaybe, MOTDMaybe, mapSeed, dimension;
+@end
+
+@implementation SCHandshake
++(uint8_t)packetId; { return 0x02; }
+@synthesize connectionHash;
+@end
+
+@implementation SCChatMessage
++(uint8_t)packetId; { return 0x03; }
+@synthesize message;
+@end
+
+@implementation SCTimeUpdate
++(uint8_t)packetId; { return 0x04; }
+@synthesize timeInMinutes;
+@end
+
+Class MinecraftMessageFactorySC(uint8_t packetId)
+{
+	static Class messages[0xff] = {Nil};
+	static BOOL inited = NO;
+	if(!inited) {
+		insert(SCKeepAlive);
+		insert(SCLoginResponse);
+		insert(SCHandshake);
+		insert(SCChatMessage);
+		insert(SCTimeUpdate);
 		inited = YES;
 	}
 	return messages[packetId];
